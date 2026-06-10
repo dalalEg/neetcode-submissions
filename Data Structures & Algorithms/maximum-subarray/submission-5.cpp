52c451d650a96040f8825033fb0d0d93a2d0d2c6
dalalEg <49488556+dalalEg@users.noreply.github.com>
2026-06-10 18:58:41 +0300

    Add: maximum-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-subarray/submission-5.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curr=nums[0],res=nums[0];
+        for(int i= 1;i<nums.size();i++){
+            curr=max(curr+nums[i],nums[i]);
+            res=max(res,curr);
+        }
+        return res;
+    }
+};
