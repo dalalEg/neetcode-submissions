7794b0759dfff8b3acb8a308a3f187d45fdd017a
dalalEg <49488556+dalalEg@users.noreply.github.com>
2026-07-16 17:18:01 +0300

    Add: open-the-lock - submission-1

---
+++ Data Structures & Algorithms/open-the-lock/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if target =="0000":
+            return 0
+        visited=set(deadends)
+        if "0000"  in visited:
+            return -1
+        dq=deque()
+        dq.append("0000")
+        visited.add("0000")
+        turns=0
+        while dq:
+            turns+=1
+            size=len(dq)
+            for _ in range(size):
+                curr=dq.popleft()
+                for i in range(4):
+                    for j in [1,-1]:
+                        digit = str((int(curr[i]) + j) % 10)
+                        nextLock = curr[:i] + digit + curr[i+1:]
+                        if nextLock in visited:
+                            continue
+                        if nextLock == target:
+                            return turns
+                        dq.append(nextLock)
+                        visited.add(nextLock)
+        return -1
